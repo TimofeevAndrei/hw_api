fb4901161c2e324e6a77cb43989f0b7f7e0cd229
TimofeevAndrei <i.pika4u@gmail.com>
2022-11-14 23:40:42 +0700

    one task done, now done!

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 import requests
 from pprint import pprint
+#Самый умный из троих: Hulk, Captain America, Thanos.
 
 url = requests.get('https://akabab.github.io/superhero-api/api/all.json')
 res = json.loads(url.text)
@@ -9,8 +10,19 @@ for x in res:
     name = x.get('name')
     pwrst = x.get('powerstats')
     superheroes.update({name: pwrst.get('intelligence')})
-# pprint(superheroes)
 
-max_value = max(superheroes.values())
-top_intelligence = {k: v for k, v in superheroes.items() if v == max_value}
-pprint(top_intelligence)
+top_heroes = []
+heroes = ['Hulk', 'Thanos', ' Captain America']
+for item in superheroes.items():
+    name, intell = item
+    for i in heroes:
+        if name == i:
+            top_heroes.append(list(item))
+
+top_heroes.sort(reverse=True)
+print(top_heroes[0][0])
+
+
+
+# Задание №2
+
